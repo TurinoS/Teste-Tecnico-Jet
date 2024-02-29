ad66dc9ba70d0791e86312d9ccb81d3ac2de61fc
Paulo Turino <paulo.turino01@gmail.com>
2024-02-29 11:48:10 +0100

    Implementação do table head

---
+++ src/app/components/TableHead.tsx
@@ -0,0 +1,13 @@
+export default function TableHead() {
+  return (
+    <div className="table-header-group bg-gray-50">
+      <div className="table-row">
+        <div className="table-cell text-left p-4 w-[25%]">CAR</div>
+        <div className="table-cell text-left p-4 w-[25%]">NEXT RESERVATION</div>
+        <div className="table-cell text-left p-4 w-[25%]">STATUS</div>
+        <div className="table-cell text-left p-4 w-[25%]">RATING</div>
+        <div className="table-cell text-left p-4">ACTIONS</div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Container from "./components/Container";
+import TableHead from "./components/TableHead";
 
 export default function Home() {
   return (
     <main>
       <Container>
-        <h1 className="px-4 py-1">Hello, World!</h1>
+        <TableHead />
       </Container>
     </main>
   );
